6787c741d13ad0e14e7cbe94d6fb491d1d890270
Christopher McKenzie <camckenzie94@gmail.com>
2022-01-31 18:53:35 -0500

    added buttons

--- player.py
+++ player.py
@@ -1,11 +1,17 @@
 from tkinter import *
 import pygame
 
-
 root = Tk()
 root.title('Music Player')
 root.iconbitmap('images/dj.ico')
 root.geometry('500x300')
 
+# Initialize Pygame Mixer
+pygame.mixer.init()
+
+# Create Playlist Box
+
+song_box = Listbox(root, bg='black', fg='green', width=60)
+song_box.pack(pady=20)
 
 root.mainloop()
