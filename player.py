9a24af7d18cc820ad3cbc43bbeea7a34eddcb19a
Christopher McKenzie <camckenzie94@gmail.com>
2022-02-01 16:19:45 -0500

    Added next, prev, delete, and delete all functionalities

--- player.py
+++ player.py
@@ -14,21 +14,36 @@ pygame.mixer.init()
 
 # Add song function
 def add_song():
+    '''Add song function'''
+
     # What directory to look in initially,
     song = filedialog.askopenfilename(
         initialdir='music/', title='Choose A Song', filetypes=(('mp3 Files', '*.mp3'), ))
     # Strip out the directory info and .mp3 extension from song name
-    song = song.replace(
-        'C:/Users/Chris/Documents/GitHub/music-player/music/', '')
+    song = song.split('/')[-1]
     song = song.replace('.mp3', '')
     # Add song to song box
     # where to insert, what to insert
     song_box.insert(END, song)
 
-# Play selected song
+
+def add_multiple_songs():
+    '''Add multiple songs function'''
+
+    songs = filedialog.askopenfilenames(
+        initialdir='music/', title='Choose A Song', filetypes=(('mp3 Files', '*.mp3'), ))
+
+    # Loop through song list and replace diretory info and mp3
+    for song in songs:
+        song = song.split('/')[-1]
+        song = song.replace('.mp3', '')
+        # Insert into playlist
+        song_box.insert(END, song)
 
 
 def play():
+    '''Plays the selected song'''
+
     # Active = the one clicked
     song = song_box.get(ACTIVE)
     song = f'C:/Users/Chris/Documents/GitHub/music-player/music/{song}.mp3'
@@ -36,12 +51,89 @@ def play():
     pygame.mixer.music.load(song)
     pygame.mixer.music.play(loops=0)
 
-# Stop playing current song
-
 
 def stop():
+    '''Stops currently playing song'''
+
     pygame.mixer.music.stop()
     song_box.selection_clear(ACTIVE)
+
+
+def next():
+    '''Play the next song in the playlist'''
+    # Gives us current song playing in playlist in form of tuple (0,)
+    next_song = song_box.curselection()
+    # Add one to current song number
+    next_song = next_song[0]+1
+    # Grab song title from playlist
+    song = song_box.get(next_song)
+
+    # This is the entire play song function
+    song = f'C:/Users/Chris/Documents/GitHub/music-player/music/{song}.mp3'
+    pygame.mixer.music.load(song)
+    pygame.mixer.music.play(loops=0)
+
+    # Move active bar in playlist box
+    song_box.selection_clear(0, END)
+
+    # Activate new song bar
+    song_box.activate(next_song)
+
+    # Set Active bar to Next Song
+    song_box.selection_set(next_song, last=None)
+
+
+def prev():
+    '''Play the previous song in the playlist'''
+    next_song = song_box.curselection()
+    next_song = next_song[0]-1
+    song = song_box.get(next_song)
+
+    # This is the entire play song function
+    song = f'C:/Users/Chris/Documents/GitHub/music-player/music/{song}.mp3'
+    pygame.mixer.music.load(song)
+    pygame.mixer.music.play(loops=0)
+
+    song_box.selection_clear(0, END)
+    song_box.activate(next_song)
+    song_box.selection_set(next_song, last=None)
+
+
+def delete():
+    '''Delete a song'''
+
+    # Anchor is the highlighted song
+    song_box.delete(ANCHOR)
+    pygame.mixer.music.stop()
+
+
+def delete_all():
+    '''Delete all songs from playlist'''
+
+    song_box.delete(0, END)
+    pygame.mixer.music.stop()
+
+
+# When we click pause, we need to know whether or not song has been paused or not
+# Need to pass in this ability to know
+# Create global pause variable
+global paused
+paused = False
+
+
+def pause(is_paused):
+    '''Pause and Unpause the current song'''
+    # Want this global variable inside and outside function
+    # So it they global variable is consistent inside and out the function
+    global paused
+    paused = is_paused
+
+    if paused:
+        pygame.mixer.music.unpause()
+        paused = False
+    else:
+        pygame.mixer.music.pause()
+        paused = True
 
 
 # Create Playlist Box
@@ -61,10 +153,11 @@ controls_frame = Frame(root)
 controls_frame.pack()
 
 # Create Player Control Buttons
-prev_btn = Button(controls_frame, image=prev_img, borderwidth=0)
-next_btn = Button(controls_frame, image=next_img, borderwidth=0)
+prev_btn = Button(controls_frame, image=prev_img, borderwidth=0, command=prev)
+next_btn = Button(controls_frame, image=next_img, borderwidth=0, command=next)
 play_btn = Button(controls_frame, image=play_img, borderwidth=0, command=play)
-pause_btn = Button(controls_frame, image=pause_img, borderwidth=0)
+pause_btn = Button(controls_frame, image=pause_img,
+                   borderwidth=0, command=lambda: pause(paused))
 stop_btn = Button(controls_frame, image=stop_img, borderwidth=0, command=stop)
 
 prev_btn.grid(row=0, column=0, padx=10)
@@ -81,5 +174,17 @@ root.config(menu=my_menu)
 add_song_menu = Menu(my_menu)
 my_menu.add_cascade(label='Add Songs', menu=add_song_menu)
 add_song_menu.add_command(label='Add One Song To PlayList', command=add_song)
+
+# Add multiple songs to playlist
+add_song_menu.add_command(
+    label='Add Multiple Songs To PlayList', command=add_multiple_songs)
+
+# Create delete song menu
+remove_song_menu = Menu(my_menu)
+my_menu.add_cascade(label='Remove Songs', menu=remove_song_menu)
+remove_song_menu.add_command(
+    label='Delete A Song From Playlist', command=delete)
+remove_song_menu.add_command(
+    label='Delete All Songs From Playlist', command=delete_all)
 
 root.mainloop()
